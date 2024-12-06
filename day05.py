b3c2e23c2acbf3c13a77fecca1b90a31d4cbf626
Mark Thebridge <mark.thebridge@gmail.com>
2024-12-06 13:20:12 +0000

    Day5 part2

--- day05.py
+++ day05.py
@@ -9,12 +9,34 @@ def parse_line(line: str) -> tuple[int, int]:
 
 
 def check_valid(pages: list[str], rule_map: dict[str, str]) -> bool:
-    # Validity is only broken if
+    # Check pairwise.  Since map holds before -> after direction,
+    # check for violations by looking for any of the preceding elements.
     for i, v in enumerate(pages):
-        for j in range(i):
-            if pages[j] in rule_map[v]:
-                return False
+        if any(pages[j] in rule_map[v] for j in range(i)):
+            return False
     return True
+
+
+def sort_pages(pages: list[str], rule_map: dict[str, str]) -> list[str]:
+    # Topological DFS sort.  Lifted straight from Wikipedia.
+    # This actually sorts in reverse order, but since we only need
+    # the middle item this is fine.
+    out = []
+
+    def visit(page: str):
+        if page not in pages:
+            return
+
+        for next in rule_map[page]:
+            visit(next)
+
+        pages.remove(page)
+        out.append(page)
+
+    while pages:
+        visit(pages[0])
+
+    return out
 
 
 def run(input: str) -> tuple[int, int]:
@@ -25,12 +47,15 @@ def run(input: str) -> tuple[int, int]:
         rule_map[a].append(b)
 
     part1 = 0
+    part2 = 0
     for u in updates.splitlines():
         pages = u.split(",")
         if check_valid(pages, rule_map):
             part1 += int(pages[len(pages) // 2])
+        else:
+            sorted_pages = sort_pages(pages, rule_map)
+            part2 += int(sorted_pages[len(sorted_pages) // 2])
 
-    part2 = 0
     return part1, part2
 
 
