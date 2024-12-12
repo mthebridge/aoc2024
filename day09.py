d79b4b59689db05809d1e68237fd3bf5992bbcca
Mark Thebridge <mark.thebridge@gmail.com>
2024-12-12 15:47:41 +0000

    Tidying

--- day09.py
+++ day09.py
@@ -1,6 +1,9 @@
 """
-Brute force algorithm that's pretty memory efficient - only stores one mutable copy of the input plus constant factor.
-Could do something cleverer but part 2 runs in 5 seconds.
+Brute force algorithm that's slow but pretty memory efficient, as
+it only stores one mutable copy of the input plus constant factor.
+
+Main fun was keeping track of all the pointers and so on correctly.
+Could do something cleverer but part 2 runs in 5 seconds...
 """
 
 import copy
@@ -14,7 +17,8 @@ def get_file_data(input: list[int], idx: int) -> tuple[int, int]:
 
 
 def calc_csum(file_id: int, file_size: int, disk_pos: int) -> int:
-    return sum((file_id * (disk_pos + i) for i in range(file_size)))
+    # The checksum is file_id * (sum of disk positions), which is an arithemtic sequence
+    return file_size * ((file_id * disk_pos) + (file_id * (disk_pos + file_size - 1))) // 2
 
 
 def defrag(original_input: list[int], entire_files: bool) -> int:
@@ -32,13 +36,16 @@ def defrag(original_input: list[int], entire_files: bool) -> int:
 
     while start_ptr <= end_ptr:
         if start_ptr % 2 == 0:
-            # Existing file.
+            # Existing file.  Add checksum and move disk forward.
             start_id, start_size = get_file_data(input, start_ptr)
-            start_id, orig_start_size = get_file_data(original_input, start_ptr)
             if start_size > 0:
                 # print(f"Add checksum for file {start_id} at {disk_pos}")
                 checksum += calc_csum(start_id, start_size, disk_pos)
-            disk_pos += max(start_size, orig_start_size)
+            else:
+                # We need to check how big the *original* file in this space was so we know
+                # how far to advance the disk
+                _, start_size = get_file_data(original_input, start_ptr)
+            disk_pos += start_size
             start_ptr += 1
         else:
             cur_end_ptr = end_ptr
@@ -50,15 +57,15 @@ def defrag(original_input: list[int], entire_files: bool) -> int:
 
             if entire_files:
                 # No space for the whole file.  Keep moving backwards until we find one that does fit.
-                # Start from the end until we find a non-zero file that can move
+                # Start from the end until we find a non-zero-sized file that can move
                 while (
                     end_size > cur_space or end_size == 0
                 ) and cur_end_ptr > start_ptr:
                     # print(cur_space, end_ptr, cur_end_ptr, end_size)
                     cur_end_ptr -= 2
                     end_id, end_size = get_file_data(input, cur_end_ptr)
-                # We either have found something, or this file can't go anywhere.
-                # print(start_ptr, cur_end_ptr, end_ptr)
+
+                # We either have found a space, or this file can't go anywhere.
                 if end_size > cur_space or cur_end_ptr <= start_ptr:
                     # Nothing can fit in this space.  Move the disk up, and reset end_size.
                     # print(f"No files fit at {start_ptr}, increment disk by {cur_space}")
@@ -66,15 +73,10 @@ def defrag(original_input: list[int], entire_files: bool) -> int:
                     cur_space = 0
                     end_size = 0
 
-                # else:
-                #     print(f"File at {cur_end_ptr} size {end_size} moved to {cur_space} at {start_ptr}")
-
             if cur_space != 0:
                 to_move = min(cur_space, end_size)
-                # print(f"Add checksum for file {end_id} at {disk_pos}")
                 checksum += calc_csum(end_id, to_move, disk_pos)
                 disk_pos += to_move
-                # print(f"Part2: {entire_files}, ID {end_id} moving size {to_move} at {cur_end_ptr} to {start_ptr}, {cur_space}, max end {end_ptr}")
                 cur_space -= to_move
                 end_size -= to_move
                 input[cur_end_ptr] = end_size
